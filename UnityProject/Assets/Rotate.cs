6bf01c390324bec0d7f232a68aa13eff3e4fa502
Aki Latvamäki <aki.latvamaki@gmail.com>
2015-03-20 13:33:51 +0200

    Aki commented something
    Added comments

--- UnityProject/Assets/Rotate.cs
+++ UnityProject/Assets/Rotate.cs
@@ -8,6 +8,7 @@ public class Rotate : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	// Aki Commenting Here
 	void Update () {
 		angle += Time.deltaTime*10.0f;
 		Quaternion newRot = Quaternion.Euler(0.0f,angle,0.0f);
