c3341e20a402b1dd624c5a8537aa1dba9153a16d
pnisula <nisula.pekka@gmail.com>
2015-03-20 12:48:54 +0200

    First Project

---
+++ UnityProject/Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	private float angle;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angle += Time.deltaTime*10.0f;
+		Quaternion newRot = Quaternion.Euler(0.0f,angle,0.0f);
+		this.transform.rotation = newRot;
+	}
+}
